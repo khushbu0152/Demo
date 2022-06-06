fbdcd20847fd59a3ad6122d3b413f91e226097c2
kartik <kartik@toneop.com>
2022-06-06 14:20:28 +0530

    changes in index.html

--- myproject/settings.py
+++ myproject/settings.py
@@ -138,6 +138,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# MEDIA_ROOT = '/media/tecocraft/work/tecocraft project/myproject/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
